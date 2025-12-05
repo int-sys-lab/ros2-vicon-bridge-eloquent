d2e375aa1697d73951d7884c72878272c9a299ee
Logan <giantpandagod@gmail.com>
2025-12-04 20:15:47 -0500

    updated connection info + fixed for eloquent

--- launch/all_segments.launch.py
+++ launch/all_segments.launch.py
@@ -3,14 +3,14 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
 
-    vicon_computer_ip = "192.168.1.115" # ip address of vicon computer
+    vicon_computer_ip = "192.168.0.101" # ip address of vicon computer
     port_number = "801" # default port number
     host_name = f"{vicon_computer_ip}:{port_number}"
 
     return LaunchDescription([
         Node(
             package='vicon_bridge',
-            executable='vicon_bridge',
+            node_executable='vicon_bridge',
             name='vicon_bridge',
             parameters = [
                 {"host_name": host_name},
